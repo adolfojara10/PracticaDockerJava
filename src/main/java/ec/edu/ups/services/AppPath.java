1e46325b8a1023243a34a9af77a52a0c41340fb3
Romel Avila <ravilaf1@est.ups.edu.ec>
2022-05-19 12:40:59 -0500

    creacion metodos rest

---
+++ src/main/java/ec/edu/ups/services/AppPath.java
@@ -0,0 +1,9 @@
+package ec.edu.ups.services;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("webProy")
+public class AppPath extends Application {
+
+}
--- src/main/java/ec/edu/ups/services/PersonaREST.java
+++ src/main/java/ec/edu/ups/services/PersonaREST.java
@@ -1,16 +1,43 @@
 package ec.edu.ups.services;
 
+import java.util.List;
+
 import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 
 import ec.edu.ups.sd.business.PersonaON;
+import ec.edu.ups.sd.model.Persona;
 
-@Path("cuenta")
+@Path("persona")
 public class PersonaREST {
 	
 	@Inject
 	private PersonaON perON;
 	
+	@GET
+	@Path("listaPersonas")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Persona> getPersona() {
+		return perON.getClientes();
+	}
 	
+	@POST
+	@Path("crear")
+	public void calcular(@QueryParam("id") int id, @QueryParam("cedula") String cedula, @QueryParam("nombre") String nombre, @QueryParam("apellido") String apellido, @QueryParam("email") String email) {
+		Persona p= new Persona();
+		p.setId(id);
+		p.setCedula(cedula);
+		p.setNombre(nombre);
+		p.setApellido(apellido);
+		p.setEmail(email);
+		perON.guardarCliente(p);
+		//return resultado;
+
+	}
 
 }
